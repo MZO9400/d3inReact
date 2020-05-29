ae6dc5919bba1b57024cbdefeeaef8c46288a817
MZO9400 <hamza.hameed00@yahoo.com>
2020-05-30 04:45:27 +0500

    D3 fun!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let d3 = window.d3;
+
+interface State {
+    count: number;
+}
+interface DATA {
+    cx: number,
+    cy: number,
+    r: number,
+    color: string
+}
+const getRandomColor: Function = (): string => "#" + ((1 << 24) * Math.random() | 0).toString(16)
+const getRandomData: Function = (count: number) : DATA[] => {
+    let data: DATA[] = [];
+    for (let i = 0; i < count; i++) {
+        data.push({
+            cx: (Math.random() * 10000) % window.innerWidth,
+            cy: (Math.floor(Math.random() * window.innerHeight)),
+            r: (Math.random() * 25) + 10,
+            color: getRandomColor()
+        })
+    }
+    return data;
+}
+
+class App extends React.Component<{}, State> {
+    state: State = {
+        count: 0
+    }
+    svg!: SVGSVGElement;
+    componentDidUpdate = (): void => {
+        let data: DATA[] = getRandomData(this.state.count);
+        let value = d3.select(this.svg)
+            .selectAll("circle")
+            .data(data);
+        value
+            .enter()
+            .append('circle')
+            .style('fill', i => i.color)
+            .attr('cx', Math.random() * window.innerWidth )
+            .attr('cy', (Math.floor(Math.random() * window.innerHeight)))
+            .merge(value as any)
+            .transition()
+            .duration(Math.floor(Math.random() * 3000))
+            .style('fill', i => i.color)
+            .attr('cx', i => i.cx)
+            .attr('cy', i => i.cy)
+            .attr('r', i => i.r)
+        value
+            .exit()
+            .transition()
+            .duration(Math.floor(Math.random() * 3000))
+            .attr('r', 0)
+            .remove();
+
+    }
+
+    render = (): React.ReactNode => {
+        return <svg
+            width={window.innerWidth}
+            height={window.innerHeight}
+            ref={(ref: SVGSVGElement) => this.svg = ref}
+            tabIndex={0}
+            onKeyDown={(e) => {
+                let value: number;
+                switch (e.key) {
+                    case "-":
+                        value = -1;
+                        break;
+                    case "+":
+                    case "=":
+                        value = 1;
+                        break;
+                    default:
+                        value = 0;
+                }
+                this.setState(state => {
+                    return {
+                        "count": state.count + value
+                    }
+                })
+            }}
+        />;
+    }
 }
 
 export default App;
